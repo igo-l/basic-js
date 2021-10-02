6b754e46a068cadd1c19bc808da4c3b906de62c2
igo-l <igolllf@gmail.com>
2021-10-02 09:08:18 +0300

    feat: Recursive depth calculator, Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,20 +2,40 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater( str, options ) {
+  let add = '';
+  let result = '';
+  let repeatTimes = options.repeatTimes || 1;
+  let additionSeparator = options.additionSeparator || "|";
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let separator = options.separator || "+";
+  let addition = options.addition === undefined ? "" : options.addition;
+  for (let i = 0; i < repeatTimes; i++) {
+    result += str;
+    add = "";
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      add += addition;
+      if (j !== additionRepeatTimes - 1) {
+        add += additionSeparator;
+      }
+    }
+    result += add;
+    if (i !== repeatTimes - 1) {
+      result += separator;
+    }
+  }
+  return result;
 }
