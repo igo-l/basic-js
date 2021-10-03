d1b2737bf90e5ce24bcda00babfb7467ee63d83b
igo-l <igolllf@gmail.com>
2021-10-03 21:05:45 +0300

    feat: (ST) Minesweeper

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,43 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper ( matrix ) {
+  let result = [];
+   for (let i = 0; i < matrix.length; i++){
+      result[i] = matrix[i].slice();
+    }
+    result = result.map((item) => item.map((i) => 0));
+    for (let i = 0; i < matrix.length; i++){
+      for (let j = 0; j < matrix[i].length; j++){
+        if (matrix[i - 1] != undefined){
+          if (matrix[i - 1][j - 1] == true){
+            result[i][j]++;
+          }
+          if (matrix[i - 1][j] == true){
+            result[i][j]++;
+          }
+          if (matrix[i - 1][j + 1] == true){
+            result[i][j]++;
+          }
+        }
+        if (matrix[i + 1] != undefined){
+          if (matrix[i + 1][j - 1] == true){
+            result[i][j]++;
+          }
+          if (matrix[i + 1][j] == true){
+            result[i][j]++;
+          }
+          if (matrix[i + 1][j + 1] == true){
+            result[i][j]++;
+          }
+        }
+        if (matrix[i][j - 1] == true){
+          result[i][j]++;
+        }
+        if (matrix[i][j + 1] == true){
+          result[i][j]++;
+        }
+      }
+    }
+    return result;
 }
